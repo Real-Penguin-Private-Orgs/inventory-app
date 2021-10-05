91c7ed9c7b15b4cf3d38e6cfadac77db5f377917
RealPenguin <mrjanggo858@gmail.com>
2021-10-06 02:20:49 +0700

    Update Company.jsx

--- components/Company.jsx
+++ components/Company.jsx
@@ -20,7 +20,7 @@ export default function Company({ company }) {
         </div>
         <div className={style.company_card_btn_content}>
               <Link href="/company/[id]" as={`/company/${company.id}`}  passHref>
-                    <Button onClick={() => Router.push('/p/[id]', `/p/${post.id}`)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    <Button>
                           Visit {company.name}
                       </Button>
               </Link>
